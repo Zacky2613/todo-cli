c10c2e098f2fdfdfbdf750e79ccc40c4bfa9e772
Zacky2613 <zackycuber@gmail.com>
2023-02-18 23:09:59 +1000

    Re-arranged some of the inputs

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from modules.display import *
 from modules.tasks import *
 import json
 
-
 with open("data/data.json", "r") as f:
     task_data = json.load(f)
+
 
 
 # Loading any previously existing tasks.
@@ -19,14 +19,9 @@ if __name__ == "__main__":
         print(task_data)
         print("\n[1] - Add new todo")
         print("[2] - Move existing todo")
-        print("[3] - Clear every todo")
-        print("[4] - Clear every todo in a certain category")
+        print("[3] - Clear every todo in a certain category")
+        print("[4] - Clear every todo")
         print("[0] - Exit\n")
 
         user_input = input("Input: ").lower()
         handle_commands(task_input=user_input, task_data=task_data)
-
-
-
-
-    
