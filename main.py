2ae703d6cd70904fc87b0c0b8b9c5126f44f471c
Zacky2613 <zackycuber@gmail.com>
2023-02-20 22:01:29 +1000

    Removed unused varibales and debugging infomation.

--- main.py
+++ main.py
@@ -6,17 +6,12 @@ with open("data/data.json", "r") as f:
     task_data = json.load(f)
 
 
-
-# Loading any previously existing tasks.
-task_todo = [task_data["todo"]]
-task_doing = [task_data["doing"]]
-task_done = [task_data["done"]]
-
-
 if __name__ == "__main__":
+    # We do it here because the other commands have this preloaded-
+    # -for showing exception errors in their function.
     while True:
         display_todos(data=task_data)
-        print(task_data)
+
         print("\n[1] - Add new todo")
         print("[2] - Move existing todo")
         print("[3] - Clear every todo in a certain category")
